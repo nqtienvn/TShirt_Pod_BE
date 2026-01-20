5ba75aa95211eebe84a3fa10f223bbe6c48523ef
nqtienvn <quangtiencoder1876@gmail.com>
2026-01-20 21:44:41 +0700

    Initial commit

---
+++ src/main/java/com/shirt/pod/PodBeApplication.java
@@ -0,0 +1,13 @@
+package com.shirt.pod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PodBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shirt/pod/PodBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shirt.pod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

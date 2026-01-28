326461707faba4442d943e4372d12034a6bcd75f
nqtienvn <quangtiencoder1876@gmail.com>
2026-01-28 14:44:34 +0700

    BE: fix mapper(#5)

--- src/main/java/com/shirt/pod/mapper/OrderMapper.java
+++ src/main/java/com/shirt/pod/mapper/OrderMapper.java
@@ -3,15 +3,12 @@ package com.shirt.pod.mapper;
 import com.shirt.pod.model.dto.response.OrderDTO;
 import com.shirt.pod.model.entity.Order;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
 
-    @Mapping(target = "createdDate", expression = "java(order.getCreatedDate() != null ? " +
-            "order.getCreatedDate().atZone(java.time.ZoneId.systemDefault()).toLocalDateTime() : null)")
     OrderDTO toDTO(Order order);
 
     List<OrderDTO> toDTOList(List<Order> orders);
--- src/main/java/com/shirt/pod/model/dto/response/OrderDTO.java
+++ src/main/java/com/shirt/pod/model/dto/response/OrderDTO.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 import java.time.LocalDateTime;
 
 @Getter
@@ -23,5 +24,5 @@ public class OrderDTO {
     private String paymentStatus;
     private String note;
     private Long userId;
-    private LocalDateTime createdDate;
+    private Instant createdDate;
 }
